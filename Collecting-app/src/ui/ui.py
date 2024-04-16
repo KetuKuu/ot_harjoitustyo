b02529597e9cdc19c8989cabc2b5642f7078f566
kety <kety.kuusaru@helsinki.fi>
2024-04-16 16:31:31 +0300

    Päivitetty

---
+++ Collecting-app/src/ui/ui.py
@@ -0,0 +1,80 @@
+from ui.login_view import LoginView
+from ui.create_user_view import CreateUserView
+from ui.user_view import UserView
+#from ui.add_view import AddView
+#from ui.project_view import ProjectView
+
+class UI:
+    #Sovelluksen käyttöliittymästä vastaava luokka
+    def __init__(self, root):
+
+        self._root = root
+        self._current_view = None
+
+    def start(self):
+        self._show_login_view()    
+        
+    def _hide_current_view(self): 
+        if self._current_view:   
+            self._current_view.destroy()
+
+        self._current_view = None
+    
+    def _show_login_view(self):
+        self._hide_current_view()
+        print("_show_login_view method")
+
+
+        self._current_view=LoginView(
+            self._root,
+            self._show_user_view, 
+            self._show_create_user_view
+            )
+        self._current_view.pack()
+
+    def _show_create_user_view(self):
+        self._hide_current_view()
+        print(" _show_create_user_view")
+
+        self._current_view = CreateUserView(
+            self._root,
+            self._show_project_view,
+            self._show_login_view
+        )
+
+        self._current_view.pack()
+    
+    
+        
+    def _show_user_view(self):
+        self._hide_current_view()
+
+        self._current_view=UserView(
+            self._root,
+            self._show_login_view, 
+            self._show_add_view,
+            self._show_project_view)
+        self._current_view.pack()
+
+    def _show_add_view(self):
+        self._hide_current_view()
+
+        self._current_view=AddView(
+            self._root,
+            self._show_user_view, 
+            self._show_project_view 
+            )
+        self._current_view.pack()
+
+
+    def _show_project_view(self):
+        self._hide_current_view()
+
+        self._current_view= UserView(
+            self._root,
+            self._show_login_view, 
+            self._show_add_view
+            )
+        self._current_view.pack()
+   
+
