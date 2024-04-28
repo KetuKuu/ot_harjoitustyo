39bf6a810e33a2c06d589814aa8cc3e2f478b93e
kety <kety.kuusaru@helsinki.fi>
2024-04-28 21:44:51 +0300

    import CollectingRepository

--- Collecting-app/src/index.py
+++ Collecting-app/src/index.py
@@ -2,8 +2,7 @@ from tkinter import Tk
 from ui.ui import UI
 from initialize_database import initialize_database
 from repositories.user_repository import user_repository
-
-
+from repositories.collecting_repository import CollectingRepository
 
 
 def main():
