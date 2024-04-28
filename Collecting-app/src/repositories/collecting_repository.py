7d5a82b856d3fedfeddbf1b27b0f34a61839145a
kety <kety.kuusaru@helsinki.fi>
2024-04-28 22:18:54 +0300

    class CollectingRepository

--- Collecting-app/src/repositories/collecting_repository.py
+++ Collecting-app/src/repositories/collecting_repository.py
@@ -3,3 +3,12 @@ from database_connection import get_database_connection
 class CollectingRepository:
     def __init__(self, connection):
         self._connection = connection
+
+    def add_phone(self, image, series, model_year, price):
+        cursor = self._connection.cursor()
+        cursor.execute('''
+            INSERT INTO phones (image, series, model_year, price) 
+            VALUES (?, ?, ?, ?)
+        ''', (image, series, model_year, price))
+        self._connection.commit()
+        print("New phone added")
