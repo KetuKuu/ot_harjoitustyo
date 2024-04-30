5369456bd95f5e09b338592fd027e8ddefdf9ddc
kety <kety.kuusaru@helsinki.fi>
2024-04-30 12:43:48 +0300

    add_phone, fetch_data fixed

--- Collecting-app/src/services/collecting_service.py
+++ Collecting-app/src/services/collecting_service.py
@@ -1,5 +1,30 @@
 import os
 from repositories.collecting_repository import collecting_repository 
 
-class TransactionService:
-    def __init__(self, collecting_repository ):
+class CollectingService:
+ 
+    def __init__(self, collecting_repository):
+        self._collecting_repository = collecting_repository
+    
+    def add_phone(self, phone_data):
+
+        required_fields = ['image', 'series', 'model_year', 'price']
+        if not all(field in phone_data for field in required_fields):
+            raise ValueError("All fields must be provided.")
+     
+        self._collecting_repository.add_phone(phone_data)
+
+
+    def fetch_data(self):
+        raw_data = self._collecting_repository.fetch_data()
+        return raw_data
+    
+    def update_phone(self, phone_id, update_data):     
+        self._collecting_repository.update_phone(phone_id, update_data)
+
+    def delete_phone(self, phone_id):
+        self._collecting_repository.delete_phone(phone_id)
+
+    
+
+collecting_service = CollectingService(collecting_repository)
