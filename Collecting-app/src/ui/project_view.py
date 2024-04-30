330e1a0ebb015003b88d2c9b22d6805bec20c757
kety <kety.kuusaru@helsinki.fi>
2024-04-30 12:39:19 +0300

    update_list added

--- Collecting-app/src/ui/project_view.py
+++ Collecting-app/src/ui/project_view.py
@@ -1,8 +1,10 @@
 from tkinter import ttk, constants, StringVar, Frame, Label, Button, Listbox
+from services.collecting_service import collecting_service
 
 class ProjectView:
-    def __init__(self, root, handle_login, handle_add):
+    def __init__(self, root, user, handle_login, handle_add):
         self._root = root
+        self.user =user
         self._handle_login = handle_login
         self._handle_add = handle_add
         self._frame = None
@@ -16,14 +18,18 @@ class ProjectView:
         self._frame.destroy()
 
     def update_list(self):
-        self._listbox.delete(0, constants.END)  # Poista vanhat arvot
-        # Haetaan uudet tiedot tietokannasta tai sovelluksen tilasta
-        updated_data = fetch_phone_data()  # Tämä on esimerkki funktiosta, joka haetaan tietokannasta
-        for item in updated_data:
-            self._listbox.insert(constants.END, item)
+        self._listbox.delete(0, 'end')
 
-
-
+        headers = f"{'Image':<20}{'Series':<15}{'Model Year':<15}{'Price':<10}"
+        self._listbox.insert(constants.END, headers)
+   
+        data = collecting_service.fetch_data() 
+        
+        for item in data:
+        # Oletetaan, että jokainen item on sanakirja, jossa on avaimet 'image', 'series', 'model_year', ja 'price'
+            formatted_row = f"{item['image']:<20}{item['series']:<15}{item['model_year']:<15}{item['price']:<10}"
+            self._listbox.insert(constants.END, formatted_row)
+        
 
     def initialize(self):
         self._frame = ttk.Frame(master=self._root)
@@ -36,10 +42,8 @@ class ProjectView:
         self._listbox = Listbox(self._frame, height=10)
         self._listbox.pack(fill=constants.BOTH, expand=True)
         
-        # Populate the listbox with sample data (This should be dynamic based on actual data)
-        sample_data = ["Nokia 3310 - 2000 - 50", "Nokia 1100 - 2003 - 30"]
-        for item in sample_data:
-            self._listbox.insert(constants.END, item)
+
+        self.update_list()
 
         # Add and Logout Buttons
         add_button = ttk.Button(self._frame, text="Lisää Uusi Puhelin", command=self._handle_add)
