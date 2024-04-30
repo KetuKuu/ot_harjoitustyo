607ecb2ae0dd3ffd4c6d4703d64182c85cec5ffd
kety <kety.kuusaru@helsinki.fi>
2024-04-30 12:46:37 +0300

    add_phone

--- Collecting-app/src/repositories/collecting_repository.py
+++ Collecting-app/src/repositories/collecting_repository.py
@@ -4,11 +4,21 @@ class CollectingRepository:
     def __init__(self, connection):
         self._connection = connection
 
-    def add_phone(self, image, series, model_year, price):
+    def add_phone(self, d):
         cursor = self._connection.cursor()
         cursor.execute('''
             INSERT INTO phones (image, series, model_year, price) 
             VALUES (?, ?, ?, ?)
-        ''', (image, series, model_year, price))
+        ''', (d["image"], d["series"], d["model_year"], d["price"]))
         self._connection.commit()
         print("New phone added")
+
+    def fetch_data(self):
+        cursor = self._connection.cursor()
+        cursor.execute('SELECT * FROM phones')
+        print("hakuu")
+        return cursor.fetchall()
+        
+    
+collecting_repository = CollectingRepository(get_database_connection())
+  
