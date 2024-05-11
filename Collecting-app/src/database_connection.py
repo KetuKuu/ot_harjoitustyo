e9041d42483db0200dc59e03996a0240ff9cbcdf
kety <kety.kuusaru@helsinki.fi>
2024-05-11 10:29:21 +0300

    database_connection_test added

--- Collecting-app/src/database_connection.py
+++ Collecting-app/src/database_connection.py
@@ -2,18 +2,27 @@ import os
 import sqlite3
 
 
-
 def get_database_connection():
-      
+
     dirname = os.path.dirname(__file__)
     database_directory = os.path.join(dirname, "data")
     db_path = os.path.join(database_directory, "database.sqlite")
 
-
     os.makedirs(database_directory, exist_ok=True)
 
-  
     connection = sqlite3.connect(db_path)
     connection.row_factory = sqlite3.Row
 
     return connection
+
+def get_database_connection_test():
+    dirname = os.path.dirname(__file__)
+    database_directory = os.path.join(dirname, "data")
+    db_path = os.path.join(database_directory, "database.sqlite")
+    
+    os.makedirs(database_directory, exist_ok=True)
+
+    connection_test = sqlite3.connect(db_path)
+    connection_test.row_factory = sqlite3.Row
+    
+    return connection_test
